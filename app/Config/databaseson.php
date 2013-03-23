0795cd07f8c8c6d398448802e310d033a8421dc9
mehmet karataş <mehmetkaratasfb@gmail.com>
2013-03-23 20:17:52 +0200

    db

---
+++ app/Config/databaseson.php
@@ -0,0 +1,26 @@
+<?php 
+class DATABASE_CONFIG { 
+//a
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'fatihalp.com',
+		'login' => 'fatihalp_1',
+		'password' => '236330',
+		'database' => 'fatihalp_fefweb2',
+		'prefix' => '',
+		'encoding' => 'utf8', 
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => '123',
+		'database' => 'abc',
+		'prefix' => '',
+		 'encoding' => 'utf8',
+	);
+}
